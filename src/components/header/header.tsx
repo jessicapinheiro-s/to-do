f5edae27e6573c83ec5a3306c60ffbdcc9aefc0c
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-18 16:00:26 -0300

    Adicionando logo ao header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
+import { SiTask } from "react-icons/si";
 
 export default  function Header () {
     return (
-        <header className=" flex flex-row items-center justify-between border px-60 py-5">
+        <header className="flex flex-row items-center justify-between border border-[#e0e0e0] px-60 py-5">
             <div className="flex flex-row items-center justify-between">
                 <div>
-                    icone
+                    <SiTask />
                 </div>
                 <div>
-                    TO DO
+                    <h1 className="font-bold text-3xl">TO DO</h1>
                 </div>
             </div>
             <nav>
-                <ul className="flex flex-row items-center justify-between">
+                <ul className="flex flex-row items-center justify-between gap-4">
                     <li><Link href='./cadastrar-tarefa'>Minhas tarefas</Link></li>
                     <li><Link href='./cadastrar-tarefa'>Cadastrar nova tarefa</Link></li>
                 </ul>
