16e8a27cf38a6a2e826927fb1dc157ea4663fd37
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-27 15:41:43 -0300

    Insolando a função que obtem dados do db e atualiza a store sequencialmente.

---
+++ src/utils/task/helpers.ts
@@ -0,0 +1,17 @@
+import { Task, useTaskStore } from "../../../stores/tasks";
+import { mapInfoBaseToApp } from "../supabase/helpers";
+
+export async function getAndUpdateStore() {
+    try {
+        const data: Task[] | [] = await mapInfoBaseToApp();
+        const tasksMaped = data.map(task => {
+            return {
+                ...task,
+                taskDate: new Date(task.taskDate).toLocaleDateString()
+            }
+        });
+        useTaskStore.getState().addTasks(tasksMaped);
+    } catch (error) {
+        throw new Error('Erro ao tentar obter tasks');
+    }
+}
